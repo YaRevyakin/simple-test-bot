cf40e2ea1f6835e358e3a0d1caaf23aa1f2cd23d
YaNoob <krabvzasade@gmail.com>
2026-01-18 21:12:59 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8226360790:AAH7DPXxvUinXEKnQBub7zExNb9uNkzaC78")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8171626569:AAHFCwYirjMnGsa-UQApf_2fmVvid1aISU0")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    result = 1 + 1
-    await update.message.reply_text(f"1 + 1 = {result}")
+    await update.message.reply_text("1 + 1 = 2")
 
 if __name__ == "__main__":
     app = Application.builder().token(BOT_TOKEN).build()
