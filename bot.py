20cc0de8768e49ff7b865a59644ffc4af3dd9ca7
YaNoob <krabvzasade@gmail.com>
2026-01-18 20:43:02 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from database import get_test_data
+import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8171626569:AAHFCwYirjMnGsa-UQApf_2fmVvid1aISU0")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    data = get_test_data()
-    message = f"Тестовые данные: {data}"
-    await update.message.reply_text(message)
+    result = 1 + 1
+    await update.message.reply_text(f"1 + 1 = {result}")
 
 if __name__ == "__main__":
     app = Application.builder().token(BOT_TOKEN).build()
