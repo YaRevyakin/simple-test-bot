e52adcde919ae1094140c4b717d5c3e58c0cc068
YaNoob <krabvzasade@gmail.com>
2026-01-18 20:45:12 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,2 @@
-import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 def get_test_data():
-    """Возвращает 3 числа: 1, 2, 3"""
     return [1, 2, 3]
-
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
